7f63697123c71fca7560a3a583cfa4bb7f336641
Jian Lin <linaom1214@mail.nwpu.edu.cn>
2024-12-03 10:22:56 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,34 +15,37 @@ def index():
         return jsonify({"error": "Invalid URL"}), 400
 
     try:
-        results = sch.search_paper(str(url), fields=["title", "abstract", "venue", "year", "authors", "tldr", "embedding", "externalIds"])
+        results = sch.search_paper(str(url), year="2023-",fields=["title", "abstract", "venue", "year", "authors", "tldr", "embedding", "externalIds"])
     except Exception as e:
         return jsonify({"error": f"Request failed: {str(e)}"}), 400
 
     if not results:
         return jsonify({"error": "No results found"}), 404
-
+    
     paper_info = []
-    for item in results:
-        title = item.title
-        authors = item.authors
-        authors = [a.name for a in authors]
-        author = ",".join(authors)  # Concatenate authors into a single string
-        abstract = item.abstract
-        year = item.year
-        citation_count = item.citationCount
-        doi_url = item.externalIds.get('doi', '')  # Assuming 'externalIds' contains a 'doi'
-
-        paper_dict = {
-            "title": title,
-            "doi": doi_url,
-            "Cite": citation_count,
-            "author": author,
-            "abstract": abstract,
-            "year": year
-        }
-        paper_info.append(paper_dict)
-
+    try:
+        for item in results:
+            title = item.title
+            authors = item.authors
+            authors = [a.name for a in authors]
+            author = ",".join(authors)  # Concatenate authors into a single string
+            abstract = item.abstract
+            year = item.year
+            citation_count = item.citationCount
+            doi_url = item.externalIds.get('doi', '')  # Assuming 'externalIds' contains a 'doi'
+            # print(title, authors, abstract, year, citation_count, doi_url)
+            paper_dict = {
+                "title": title,
+                "doi": doi_url,
+                "Cite": citation_count,
+                "author": author,
+                "abstract": abstract,
+                "year": year
+            }
+            paper_info.append(paper_dict)
+    except:
+        pass
+    print(paper_info)
     feed = {
         "version": "https://jsonfeed.org/version/1",
         "title": f"Papers of ({url})",
